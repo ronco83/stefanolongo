eb314829f48f858a67c17625591e01abc5e5ec12
Stefano Longo <stefano.longo@mashfrog.com>
2015-07-12 15:31:14 +0200

    varie

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -10,24 +10,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		$stateProvider.
 		state('home', {
 			url: '/',
-			views: {
-				"top": { templateUrl: "modules/core/views/hp-top-bar.html" },
-				"": { templateUrl: 'modules/core/views/home.client.view.html'}
-			}
-		}).
-		state('about', {
-			url: '/about-me',
-			views: {
-				"top": { templateUrl: "modules/core/views/top-bar.html" },
-				"": { templateUrl: 'modules/core/views/about.client.view.html'}
-			}
-			}).
-		state('contacts', {
-			url: '/contacts',
-			views: {
-				"top": { templateUrl: "modules/core/views/top-bar.html" },
-				"": { templateUrl: 'modules/core/views/contacts.client.view.html'}
-			}
+			templateUrl: 'modules/core/views/home.client.view.html'
 		});
 	}
 ]);
